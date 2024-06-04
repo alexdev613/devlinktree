6cbcda149fef24f3b7ef3ee771db2aa1baf01e5f
Alex Nascimento <alexjose68@hotmail.com>
2024-06-04 12:44:23 -0300

    feat: Create and style a custom Input component to be used in components, extending the standard attributes of an HTML input element to the Input. And I created the Login page layout with form and function to get the data entered in the inputs

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,53 @@
+
+import { FormEvent, useState } from 'react';
+import { Link } from "react-router-dom";
+import { Input } from '../../components/Input';
+
 export function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent){
+    e.preventDefault();
+
+    console.log({
+      email: email,
+      password: password,
+    })
+  }
+
   return (
-    <div>
-      <h1>Págima Login</h1>
+    <div className="flex w-full h-screen items-center justify-center flex-col">
+      <Link to={"/"}>
+        <h1 className="mt-11 text-white mb-7 font-bold text-5xl">Dev
+          <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Link</span>
+        </h1>
+      </Link>
+
+      <form onSubmit={handleSubmit} className="w-full max-w-xl flex flex-col px-2">
+        <Input
+          placeholder="Digite o seu email..."
+          type="email"
+          value={email}
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+
+        <Input
+          placeholder='*********************'
+          type='password'
+          value={password}
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+
+        <button
+          type="submit"
+          className="h-9 bg-blue-600 rounded border-0 text-lg font-medium"
+        >
+          Acessar
+        </button>
+      </form>
+
     </div>
   )
 }
