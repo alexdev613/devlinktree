89f85bcbd7c39a7173f0b9a222d80471b07800bb
Alex Nascimento <alexjose68@hotmail.com>
2024-06-03 21:37:19 -0300

    feat: I installed the react-icons library. I passed a general css to the application body in src/index.css. I created the basis of the Home page structure by styling with tailwindcss classes and created the Social component to implement social media icons for the Home page footer

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,39 @@
+import { Social } from '../../components/Social'
+import { FaFacebook, FaInstagram, FaYoutube, FaGithub } from 'react-icons/fa';
+
 export function Home() {
   return (
-    <div className="w-full h-screen bg-neutral-950">
-      <h1 className="text-white text-3xl">Págima Home</h1>
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Alex Nascimento</h1>
+      <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇🏼</span>
+
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+          <a>
+            <p className="text-base md:text-lg">
+              Canal no Youtube
+            </p>
+          </a>
+        </section>
+
+        <footer className="flex justify-center gap-3 my-4">
+          <Social url="https://facebook.com/">
+            <FaFacebook size={35} color='#FFF' />
+          </Social>
+
+          <Social url="https://youtube.com/">
+            <FaYoutube size={35} color='#FFF' />
+          </Social>
+
+          <Social url="https://instagram.com/alexnascimento613">
+            <FaInstagram size={35} color='#FFF' />
+          </Social>
+
+          <Social url="https://github.com/alexdev613">
+            <FaGithub size={35} color='#FFF' />
+          </Social>
+        </footer>
+      </main>
     </div>
   )
 }
