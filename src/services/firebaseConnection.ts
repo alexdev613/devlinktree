e4b58b62d7744845a5aa95f9b38a3202133a3eba
Alex Nascimento <alexjose68@hotmail.com>
2024-07-05 13:29:29 -0300

    feat: precisei configurar as variáveis de ambiente manualmente, pois os plugins específicos que conhecia não estavam mais disponíveis, para isso instalei o pacote dotenv, e configurei as variáveis de ambiente no arquivo vite.config.ts, para as variáveis poderem ser carregadas no meu arquivo .env, privando as minhas chaves de api e deixando a configuração muito mais segura! Após isso chamei as variáveis de ambiente no arquivo firebaseConection.ts conforme congigurado no vite.config.ts e ficou ok

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY as string,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN as string,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID as string,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET as string,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID as string,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID as string,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
 };
 
 const app = initializeApp(firebaseConfig);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'import.meta.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.REACT_APP_FIREBASE_API_KEY),
+    'import.meta.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+    'import.meta.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_PROJECT_ID),
+    'import.meta.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET),
+    'import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
+    'import.meta.env.VITE_FIREBASE_APP_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_APP_ID),
+  },
+});
